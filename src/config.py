42025e7115edff5f9a1eba36e49dcf16a5db5a69
StephenHB <stephen.zhang@gmail.com>
2025-06-30 17:04:51 -0400

    add knn imputation method

--- src/config.py
+++ src/config.py
@@ -4,4 +4,16 @@ KAGGLEHUB_DATASET_PATH = "parisrohan/credit-score-classification"
 
 # Local paths to CSV files
 LOCAL_TRAIN_CSV = "/Users/stephenzhang/Downloads/credit_score_classification/train.csv"
-LOCAL_TEST_CSV = "/Users/stephenzhang/Downloads/credit_score_classification/test.csv" 
+LOCAL_TEST_CSV = "/Users/stephenzhang/Downloads/credit_score_classification/test.csv"
+
+# Target column name
+TARGET_COL = "Credit_Mix"
+
+# Default features to use
+DEFAULT_FEATURES = ["Month", "Age", "Annual_Income", "Monthly_Inhand_Salary", "Num_Bank_Accounts"]
+
+# ID columns
+ID_COLS = ["Customer_ID", "ID", "Name", "SSN"]
+
+# Categorical columns
+CAT_COLS = ["Occupation", "Payment_Behaviour"] 
